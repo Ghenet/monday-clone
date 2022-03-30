fadc11556ac84206380ca1834951656f33cc353e
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-03-30 09:57:53 -0700

    adds the routes to nav, dashboard and tkt components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from './components/Nav';
+import DashBoard from "./pages/DashBoard";
+import TicketPage from './pages/TicketPage';
 
 const App = () => {
   return (
     <div className="app">
-     
+     <BrowserRouter>
+       <Nav />
+       <Routes>
+         <Route path='/' element={<DashBoard />} />
+         <Route path='/ticket' element={<TicketPage />} />
+         <Route path='/ticket/:id' element={<TicketPage editMode={true}/>} />
+       </Routes>
+     </BrowserRouter>
     </div>
   );
 }
